b1e74a78eb3579f377a1d6f3b1fb4ae328e97f89
itizawa <itizawa.pen@gmail.com>
2022-02-03 23:59:48 +0900

    ✨ display test button

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,7 @@
 import Head from 'next/head';
-import Container from '../components/container';
-import MoreStories from '../components/more-stories';
-import HeroPost from '../components/hero-post';
-import Intro from '../components/intro';
-import Layout from '../components/layout';
+
+import Button from '@nextui-org/react/button';
 import { getAllPosts } from '../lib/api';
-import { CMS_NAME } from '../lib/constants';
 import Post from '../types/post';
 
 type Props = {
@@ -13,29 +9,14 @@ type Props = {
 };
 
 const Index = ({ allPosts }: Props) => {
-  const heroPost = allPosts[0];
-  const morePosts = allPosts.slice(1);
+  console.log(allPosts);
   return (
     <>
-      <Layout>
-        <Head>
-          <title>Next.js Blog Example with {CMS_NAME}</title>
-        </Head>
-        <Container>
-          <Intro />
-          {heroPost && (
-            <HeroPost
-              title={heroPost.title}
-              coverImage={heroPost.coverImage}
-              date={heroPost.date}
-              author={heroPost.author}
-              slug={heroPost.slug}
-              excerpt={heroPost.excerpt}
-            />
-          )}
-          {morePosts.length > 0 && <MoreStories posts={morePosts} />}
-        </Container>
-      </Layout>
+      <Head>
+        <title>zawalog | Top</title>
+      </Head>
+      Top Page
+      <Button>Waiting...</Button>
     </>
   );
 };
