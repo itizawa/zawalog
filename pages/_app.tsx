f2a817658d4487d4e39cbe1702e363f07a4a68dc
itizawa <itizawa.pen@gmail.com>
2022-02-03 23:49:13 +0900

    ✨ wrap provider

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,11 @@
+import { NextUIProvider } from '@nextui-org/react';
 import { AppProps } from 'next/app';
 import '../styles/index.css';
 
 export default function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <NextUIProvider>
+      <Component {...pageProps} />
+    </NextUIProvider>
+  );
 }
