b178ac259e5826422561c2ab3a5a675e3f388f8a
itizawa <itizawa.pen@gmail.com>
2022-02-05 17:50:57 +0900

    ✨ implement navigation

--- src/components/parts/commons/Navigation/Navigation.tsx
+++ src/components/parts/commons/Navigation/Navigation.tsx
@@ -1,5 +1,25 @@
 import { VFC } from 'react';
+import { Container, Grid, Text } from '@nextui-org/react';
+import { Link } from '~/components/parts/commons/Link';
 
 export const Navigation: VFC = () => {
-  return <nav>hoge</nav>;
+  return (
+    <Container md css={{ py: '$8' }} display="flex" justify="space-between">
+      <div>
+        <Link href="/">
+          <Text color="white" b>
+            Zawalog
+          </Text>
+        </Link>
+      </div>
+      <Grid css={{ display: 'flex', gap: '$8' }}>
+        <Link href="/">
+          <Text color="white">Home</Text>
+        </Link>
+        <Link href="/product">
+          <Text color="white">Product</Text>
+        </Link>
+      </Grid>
+    </Container>
+  );
 };
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,29 @@
-import { NextUIProvider } from '@nextui-org/react';
+import { createTheme, NextUIProvider } from '@nextui-org/react';
 import { AppProps } from 'next/app';
 import 'ress';
 
+const myDarkTheme = createTheme({
+  type: 'dark',
+  theme: {
+    colors: {
+      background: '#121212',
+      text: '#cecece',
+      // you can also create your own color
+      myDarkColor: '#ff4ecd',
+      secondary: '#7856ff',
+      // ...  more colors
+    },
+    space: {},
+    fonts: {
+      sans: "-apple-system, BlinkMacSystemFont, 'Segoe UI', 'Roboto','Oxygen', 'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue', sans-serif;",
+      mono: "Menlo, Monaco, 'Lucida Console', 'Liberation Mono','DejaVu Sans Mono', 'Bitstream Vera Sans Mono'",
+    },
+  },
+});
+
 export default function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <NextUIProvider>
+    <NextUIProvider theme={myDarkTheme}>
       <Component {...pageProps} />
     </NextUIProvider>
   );
