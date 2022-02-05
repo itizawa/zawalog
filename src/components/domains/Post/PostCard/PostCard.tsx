870a5ac52dafdbceb440d16da220d81e6735ebc6
itizawa <itizawa.pen@gmail.com>
2022-02-05 18:43:55 +0900

    ✨ implement card

---
+++ src/components/domains/Post/PostCard/PostCard.tsx
@@ -0,0 +1,22 @@
+import { VFC } from 'react';
+import { Card, Col, Text } from '@nextui-org/react';
+import { Post } from '~/domains/Post';
+
+type Props = {
+  post: Post;
+};
+
+export const PostCard: VFC<Props> = ({ post }) => {
+  return (
+    <Card clickable cover>
+      <Card.Header css={{ position: 'absolute', zIndex: 1, top: 5 }}>
+        <Col>
+          <Text size={12} weight="bold" transform="uppercase" color="$text">
+            {post.title}
+          </Text>
+        </Col>
+      </Card.Header>
+      <Card.Image src={post.coverImage} height={340} width="100%" alt="Card image background" />
+    </Card>
+  );
+};
