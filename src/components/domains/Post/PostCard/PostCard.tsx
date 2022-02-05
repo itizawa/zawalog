a20b15d231d0d1f3661f1036ebaba0671300bb9b
itizawa <itizawa.pen@gmail.com>
2022-02-05 19:04:37 +0900

    ✨ dispay date

--- src/components/domains/Post/PostCard/PostCard.tsx
+++ src/components/domains/Post/PostCard/PostCard.tsx
@@ -1,6 +1,9 @@
 import { VFC } from 'react';
 import { Card, Col, Text } from '@nextui-org/react';
+import { format } from 'date-fns';
+
 import { Post } from '~/domains/Post';
+import { DATE_FORMAT } from '~/constants/dateFormat';
 
 type Props = {
   post: Post;
@@ -11,13 +14,16 @@ export const PostCard: VFC<Props> = ({ post }) => {
     <Card clickable cover>
       <Card.Header css={{ position: 'absolute', zIndex: 1, top: 5 }}>
         <Col>
-          <Text size={12} weight="bold" transform="uppercase" color="$text">
+          <Text size={12} weight="bold" transform="uppercase" color="$black">
             {post.title}
+          </Text>
+          <Text size={8} weight="bold" transform="uppercase" color="$black">
+            {format(new Date(post.date), DATE_FORMAT.EXCEPT_SECOND)}
           </Text>
         </Col>
       </Card.Header>
       {/* TODO set ogp for without coverImage */}
-      <Card.Image src={post.coverImage || '/'} height={340} width="100%" alt="Card image background" />
+      <Card.Image src={post.coverImage || '/'} height="auto" width="100%" alt="Card image background" />
     </Card>
   );
 };
