cc4d8ec20a57daa72f75d1480c0c7fdc047b4bc0
itizawa <itizawa.pen@gmail.com>
2022-02-05 18:43:40 +0900

    🔥 delte property

--- src/domains/Post.ts
+++ src/domains/Post.ts
@@ -3,36 +3,24 @@ export class Post {
   title: string;
   date: string;
   coverImage: string;
-  excerpt: string;
-  ogImage: {
-    url: string;
-  };
   content: string;
   constructor({
     slug,
     title,
     date,
     coverImage,
-    excerpt,
-    ogImage,
     content,
   }: {
     slug: string;
     title: string;
     date: string;
     coverImage: string;
-    excerpt: string;
-    ogImage: {
-      url: string;
-    };
     content: string;
   }) {
     this.slug = slug;
     this.title = title;
     this.date = date;
     this.coverImage = coverImage;
-    this.excerpt = excerpt;
-    this.ogImage = ogImage;
     this.content = content;
   }
 }
