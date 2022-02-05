e8f8938119900b4c8125106c4193f5186f46b75f
itizawa <itizawa.pen@gmail.com>
2022-02-05 18:52:46 +0900

    🏷️  change optional

--- src/domains/Post.ts
+++ src/domains/Post.ts
@@ -2,21 +2,9 @@ export class Post {
   slug: string;
   title: string;
   date: string;
-  coverImage: string;
+  coverImage?: string;
   content: string;
-  constructor({
-    slug,
-    title,
-    date,
-    coverImage,
-    content,
-  }: {
-    slug: string;
-    title: string;
-    date: string;
-    coverImage: string;
-    content: string;
-  }) {
+  constructor({ slug, title, date, coverImage, content }: Post) {
     this.slug = slug;
     this.title = title;
     this.date = date;
