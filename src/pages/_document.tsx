892ac7d563203736916a4c8a936a761f7b8c509f
itizawa <itizawa.pen@gmail.com>
2022-02-05 15:23:32 +0900

    ✨ create _documnet.tsx

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,10 +1,19 @@
-import Document, { Html, Head, Main, NextScript } from 'next/document';
+import Document, { Html, Head, Main, NextScript, DocumentContext } from 'next/document';
+import { CssBaseline } from '@nextui-org/react';
 
-export default class MyDocument extends Document {
+export class MyDocument extends Document {
+  static async getInitialProps(ctx: DocumentContext) {
+    const initialProps = await Document.getInitialProps(ctx);
+    return {
+      ...initialProps,
+      styles: <>{initialProps.styles}</>,
+    };
+  }
+
   render() {
     return (
-      <Html lang="en">
-        <Head />
+      <Html lang="js">
+        <Head>{CssBaseline.flush()}</Head>
         <body>
           <Main />
           <NextScript />
