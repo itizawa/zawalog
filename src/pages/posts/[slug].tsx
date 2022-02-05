60703a1d52d26006042542da4beb0cc13cddf3ca
itizawa <itizawa.pen@gmail.com>
2022-02-05 23:48:28 +0900

    ✨ display article

--- src/pages/posts/[slug].tsx
+++ src/pages/posts/[slug].tsx
@@ -1,8 +1,13 @@
 import { useRouter } from 'next/router';
 import ErrorPage from 'next/error';
+import Head from 'next/head';
+import { Container, Text } from '@nextui-org/react';
+import { format } from 'date-fns';
 import markdownToHtml from '~/lib/markdownToHtml';
 import { getPostBySlug, getAllPosts } from '~/lib/api';
 import { Post } from '~/domains/Post';
+import { DefaultLayout } from '~/components/parts/layouts/DefaultLayout';
+import { DATE_FORMAT } from '~/constants/dateFormat';
 
 type Props = {
   post: Post;
@@ -12,32 +17,37 @@ type Props = {
 
 const PostPage = ({ post }: Props) => {
   const router = useRouter();
+
   if (!router.isFallback && !post?.slug) {
     return <ErrorPage statusCode={404} />;
   }
+
   return (
-    <p>hoge</p>
-    // <Layout preview={preview}>
-    //   <Container>
-    //     <Header />
-    //     {router.isFallback ? (
-    //       <PostTitle>Loading…</PostTitle>
-    //     ) : (
-    //       <>
-    //         <article className="mb-32">
-    //           <Head>
-    //             <title>
-    //               {post.title} | Next.js Blog Example with {CMS_NAME}
-    //             </title>
-    //             <meta property="og:image" content={post.ogImage.url} />
-    //           </Head>
-    //           <PostHeader title={post.title} coverImage={post.coverImage} date={post.date} author={post.author} />
-    //           <PostBody content={post.content} />
-    //         </article>
-    //       </>
-    //     )}
-    //   </Container>
-    // </Layout>
+    <DefaultLayout>
+      <Head>
+        <title>Zawalog | Top</title>
+      </Head>
+      <Container xs>
+        {router.isFallback ? (
+          <Text>Loading…</Text>
+        ) : (
+          <>
+            <article className="mb-32">
+              <Head>
+                <title>{post.title}</title>
+                <meta property="og:image" content={post.coverImage} />
+              </Head>
+              <Text h1>{post.title}</Text>
+              <Text size={18} weight="bold" transform="uppercase" css={{ my: '$2' }}>
+                投稿日：{format(new Date(post.date), DATE_FORMAT.EXCEPT_SECOND)}
+              </Text>
+              <img src={post.coverImage} alt={`Cover Image for ${post.title}`} />
+              <div dangerouslySetInnerHTML={{ __html: post.content }} />
+            </article>
+          </>
+        )}
+      </Container>
+    </DefaultLayout>
   );
 };
 
