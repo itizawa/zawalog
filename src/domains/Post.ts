3d04ab4ceaabafce0d5dd36fca10166b0d0344f9
itizawa <itizawa.pen@gmail.com>
2022-02-06 23:58:02 +0900

    ✨ display description

--- src/domains/Post.ts
+++ src/domains/Post.ts
@@ -4,11 +4,13 @@ export class Post {
   date: string;
   coverImage?: string;
   content: string;
-  constructor({ slug, title, date, coverImage, content }: Post) {
+  description: string;
+  constructor({ slug, title, date, coverImage, content, description }: Post) {
     this.slug = slug;
     this.title = title;
     this.date = date;
     this.coverImage = coverImage;
     this.content = content;
+    this.description = description;
   }
 }
