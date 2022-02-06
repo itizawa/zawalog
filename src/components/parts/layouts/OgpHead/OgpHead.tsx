ec1c65ce46af1f0c0b6bd858f7115113a91662c1
itizawa <itizawa.pen@gmail.com>
2022-02-06 22:55:47 +0900

    ✨ create OgpHead

---
+++ src/components/parts/layouts/OgpHead/OgpHead.tsx
@@ -0,0 +1,34 @@
+import Head from 'next/head';
+import { VFC } from 'react';
+import { IMAGE_PATH } from '~/constants/imagePath';
+
+type Props = {
+  siteName?: string;
+  title?: string;
+  url?: string;
+  image?: string;
+  description?: string;
+  keywords?: string[];
+};
+
+const DESCRIPTION = 'Zawalog は、itizawaのブログ兼アウトプットの統合サイトです';
+const KEYWORDS = 'itizawa,個人開発';
+
+export const OgpHead: VFC<Props> = ({ siteName, title, url, image, description, keywords = [] }) => {
+  return (
+    <Head>
+      <title>{title || 'zawalog'}</title>
+      <meta name="description" content={description || DESCRIPTION} />
+      <meta name="keywords" content={keywords.length > 0 ? keywords?.join(',') : KEYWORDS} />
+      <meta property="og:site_name" content={siteName || 'zawalog'} />
+      <meta property="og:title" content={title || 'zawalog'} />
+      <meta property="og:url" content={url || 'https://www.itizaworld.com/'} />
+      <meta property="og:image" content={image || IMAGE_PATH.OGP} />
+      <meta property="og:description" content={description || DESCRIPTION} />
+      <meta name="twitter:card" content="summary_large_image" />
+      <meta name="twitter:title" content={title || 'zawalog'} />
+      <meta name="twitter:image" content={image || IMAGE_PATH.OGP} />
+      <meta name="twitter:description" content={description || DESCRIPTION} />
+    </Head>
+  );
+};
