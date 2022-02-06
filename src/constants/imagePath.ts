d9caee60e5aee0a7101cb42bfea0f7ea57bf8fce
itizawa <itizawa.pen@gmail.com>
2022-02-06 22:55:38 +0900

    ✨ create image path list file

---
+++ src/constants/imagePath.ts
@@ -0,0 +1,9 @@
+export const IMAGE_PATH = {
+  LOGO: '/images/Original.svg',
+  ERROR_404: '/images/Error_404_SVG.svg',
+  EYE_CATCH: '/images/eye-catch.svg',
+  EYE_CATCH_MOBILE: '/images/eye-catch-mobile.svg',
+  TEACH_404: '/images/Teach_SVG.svg',
+  OGP: '/assets/images/dark-logo.png',
+  NO_IMAGE: '/images/no-image.png',
+};
