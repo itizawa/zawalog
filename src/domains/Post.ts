5ec051d7ce2f98a3a918d74130327eb94f1ca032
itizawa <itizawa.pen@gmail.com>
2022-02-08 00:18:32 +0900

    🏷️ update type

--- src/domains/Post.ts
+++ src/domains/Post.ts
@@ -1,16 +1,12 @@
 export class Post {
-  slug: string;
-  title: string;
-  date: string;
-  coverImage?: string;
-  content: string;
-  description: string;
-  constructor({ slug, title, date, coverImage, content, description }: Post) {
-    this.slug = slug;
-    this.title = title;
-    this.date = date;
-    this.coverImage = coverImage;
-    this.content = content;
-    this.description = description;
+  _id: string;
+  path: string;
+  createdAt: string;
+  updatedAt: string;
+  constructor({ _id, path, createdAt, updatedAt }: Post) {
+    this._id = _id;
+    this.path = path;
+    this.createdAt = createdAt;
+    this.updatedAt = updatedAt;
   }
 }
