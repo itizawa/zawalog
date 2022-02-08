4345bd1479de03ce4d43a3bfdc6acdfdbb211c40
itizawa <itizawa.pen@gmail.com>
2022-02-08 23:40:28 +0900

    ✨ update PostCard

--- src/components/domains/Post/PostCard/PostCard.tsx
+++ src/components/domains/Post/PostCard/PostCard.tsx
@@ -4,7 +4,6 @@ import { format } from 'date-fns';
 
 import { Post } from '~/domains/Post';
 import { DATE_FORMAT } from '~/constants/dateFormat';
-import { extractTitleFromPath } from '~/lib/extractTitleFromPath';
 
 type Props = {
   post: Post;
@@ -14,10 +13,10 @@ export const PostCard: VFC<Props> = ({ post }) => {
   return (
     <Card clickable css={{ overflow: 'hidden' }} bordered>
       <Text size={24} weight="bold" color="$white">
-        {extractTitleFromPath(post.path)}
+        {post.title}
       </Text>
       <Text size={12} transform="uppercase" color="$white">
-        作成日：{format(new Date(post.createdAt), DATE_FORMAT.EXCEPT_SECOND)}
+        公開日：{format(new Date(post.publishedAt), DATE_FORMAT.EXCEPT_SECOND)}
       </Text>
       <Text size={12} transform="uppercase" color="$white">
         更新日：{format(new Date(post.updatedAt), DATE_FORMAT.EXCEPT_SECOND)}
