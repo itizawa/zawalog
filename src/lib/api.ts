8356b506cdac805b604baefd7d01375b92162f8d
itizawa <itizawa.pen@gmail.com>
2022-02-09 00:26:02 +0900

    🐛 fix build err

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -2,5 +2,5 @@ import { createClient } from 'microcms-js-sdk'; //ES6
 
 export const cmsClient = createClient({
   serviceDomain: 'itizaworld',
-  apiKey: process.env.API_KEY as string,
+  apiKey: (process.env.API_KEY as string) || '',
 });
