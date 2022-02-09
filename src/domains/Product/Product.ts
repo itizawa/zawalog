702b515f55acc34367ee5954bfccf31454cb07c9
itizawa <itizawa.pen@gmail.com>
2022-02-09 23:57:07 +0900

    🏷️ add image

--- src/domains/Product/Product.ts
+++ src/domains/Product/Product.ts
@@ -2,13 +2,15 @@ export class Product {
   id: string;
   title: string;
   url: string;
+  image: string;
   description: string;
   createdAt: string;
   updatedAt: string;
-  constructor({ id, title, url, description, createdAt, updatedAt }: Product) {
+  constructor({ id, title, url, image, description, createdAt, updatedAt }: Product) {
     this.id = id;
     this.title = title;
     this.url = url;
+    this.image = image;
     this.description = description;
     this.createdAt = createdAt;
     this.updatedAt = updatedAt;
