6507a833a8e0db7dde4157202d67cd0b08fa904b
itizawa <itizawa.pen@gmail.com>
2022-02-09 23:58:25 +0900

    ✨ create ProductCard

---
+++ src/components/domains/Product/ProductCard/ProductCard.tsx
@@ -0,0 +1,51 @@
+import { VFC } from 'react';
+import { Card, Col, Text } from '@nextui-org/react';
+import { format } from 'date-fns';
+import styled from 'styled-components';
+
+import { Product } from '~/domains/Product';
+import { DATE_FORMAT } from '~/constants/dateFormat';
+
+type Props = {
+  product: Product;
+};
+
+export const ProductCard: VFC<Props> = ({ product }) => {
+  return (
+    <StyledCard clickable cover css={{ overflow: 'hidden' }} bordered>
+      <Card.Body>
+        <Card.Image src={product.image || '/'} showSkeleton height="100%" width="100%" alt={`${product.id}-cover`} />
+      </Card.Body>
+      <Card.Footer
+        className="post-card-footer"
+        blur
+        css={{
+          position: 'absolute',
+          bgBlur: '#ffffff',
+          borderTop: '$borderWeights $light solid rgba(255, 255, 255, 0.2)',
+          zIndex: 1,
+          bottom: -200,
+        }}
+      >
+        <Col>
+          <Text size={24} weight="bold" color="$black">
+            {product.title}
+          </Text>
+          <Text size={16} weight="bold" transform="uppercase" color="$black">
+            {format(new Date(product.updatedAt), DATE_FORMAT.EXCEPT_SECOND)}
+          </Text>
+        </Col>
+      </Card.Footer>
+    </StyledCard>
+  );
+};
+
+const StyledCard = styled(Card)`
+  :hover {
+    .post-card-footer {
+      bottom: 0;
+      top: 0;
+      transition: all 0.2s ease-out;
+    }
+  }
+`;
