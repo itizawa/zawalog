2edc413d9ddc0a137a0110290a2557d6df9fccec
itizawa <itizawa.pen@gmail.com>
2022-02-15 23:06:56 +0900

    🏷️ create tags

--- src/domains/Post.ts
+++ src/domains/Post.ts
@@ -1,14 +1,18 @@
+import { Tag } from './Tags';
+
 export class Post {
   id: string;
   title: string;
   body: string;
+  tags: Tag[];
   createdAt: string;
   updatedAt: string;
   publishedAt: string;
-  constructor({ id, title, body, createdAt, updatedAt, publishedAt }: Post) {
+  constructor({ id, title, body, tags, createdAt, updatedAt, publishedAt }: Post) {
     this.id = id;
     this.title = title;
     this.body = body;
+    this.tags = tags;
     this.createdAt = createdAt;
     this.updatedAt = updatedAt;
     this.publishedAt = publishedAt;
