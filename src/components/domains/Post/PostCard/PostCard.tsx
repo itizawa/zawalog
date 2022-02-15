d52e987ca418478132d4ea00773ab3b14d7b92e2
itizawa <itizawa.pen@gmail.com>
2022-02-15 23:27:12 +0900

    ✨ display tag at post card

--- src/components/domains/Post/PostCard/PostCard.tsx
+++ src/components/domains/Post/PostCard/PostCard.tsx
@@ -4,6 +4,7 @@ import { format } from 'date-fns';
 
 import { Post } from '~/domains/Post';
 import { DATE_FORMAT } from '~/constants/dateFormat';
+import { Tag } from '~/components/parts/commons/Tag';
 
 type Props = {
   post: Post;
@@ -12,10 +13,13 @@ type Props = {
 export const PostCard: VFC<Props> = ({ post }) => {
   return (
     <Card clickable css={{ overflow: 'hidden' }} bordered>
-      <Text size={24} weight="bold" color="$white">
+      <Text size={24} weight="bold" color="$white" css={{ mb: '$2' }}>
         {post.title}
       </Text>
-      <Text size={12} transform="uppercase" color="$white">
+      {post.tags.map((v) => {
+        return <Tag key={v.id}>{v.name}</Tag>;
+      })}
+      <Text size={12} transform="uppercase" color="$white" css={{ mt: '$2' }}>
         公開日：{format(new Date(post.publishedAt), DATE_FORMAT.EXCEPT_SECOND)}
       </Text>
       <Text size={12} transform="uppercase" color="$white">
