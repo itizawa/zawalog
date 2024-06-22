7a83abb8f28eac0261fa2f8dc32eea20c1724f37
itizawa <itizawa.pen@gmail.com>
2024-06-23 00:51:01 +0900

    response を切り取って返すようにした

--- src/app/_components/domains/PostCard/PostCard.tsx
+++ src/app/_components/domains/PostCard/PostCard.tsx
@@ -7,15 +7,15 @@ import { DATE_FORMAT } from '~/constants/dateFormat';
 type Props = {
   title: string;
   publishedAt: Date;
-  body: string;
+  bodyTeaser: string;
 };
 
-export const PostCard: FC<Props> = ({ title, publishedAt, body }) => {
+export const PostCard: FC<Props> = ({ title, publishedAt, bodyTeaser }) => {
   return (
     <Card className="p-4 w-[100%]">
       <h1 className="text-2xl font-bold">{title}</h1>
       <p className="text-slate-50 text-sm mt-[8px]">公開日：{format(publishedAt, DATE_FORMAT.EXCEPT_SECOND)}</p>
-      <p className="text-slate-50 mt-2">{body}</p>
+      <p className="text-slate-50 mt-2">{bodyTeaser}</p>
     </Card>
   );
 };
